3f443f24ce44665d1b17b43e4c2d5595dc71212d
LUFENGZHAN <xiaozhan2000423@163.com>
2024-03-21 23:19:09 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,35 @@
 import path from "path";
 import fs from "fs";
 import lodash from "lodash";
-function is_file(directoryPath: string, dirPathNname: string) {
-    let file_lists = [];
-    function newList(outpath:string,dirPathNname:string ) {
+function getAllFilePaths(directoryPath: string, dirPathNname: string) {
+    let allFilePaths = []
+    function newList(outpath: string, dirPathNname: string) {
         const files = fs.readdirSync(outpath);
-        const child = []
         files.forEach((file) => {
             const newPath = path.join(outpath, file);
             const stats = fs.statSync(newPath);
             if (stats.isDirectory()) {
                 const is_file = fs.readdirSync(newPath);
                 if (is_file.length !== 0) {
-                    traverseFiles(newPath, dirPathNname);
+                    newList(newPath, dirPathNname);
                 }
                 return;
             }
             const is_same = newPath.replace(/\.[tj]s/g, "");
             if (is_same !== dirPathNname) {
-                child.push(newPath);
+                allFilePaths.push(newPath);
             }
         });
-        console.log(child,'child');
-        file_lists.push(...child);
     }
-    newList(directoryPath,dirPathNname);
-    return file_lists;
+    newList(directoryPath, dirPathNname);
+    return allFilePaths
 }
 function traverseFiles(outpath: string, dirPathNname: string) {
-    const list = is_file(outpath, dirPathNname);
-    console.log(list,'list');
+    const allFilePaths = getAllFilePaths(outpath, dirPathNname)
+    console.log(allFilePaths, 'allFilePaths',allFilePaths.length);
+
 }
-function isFolder(directoryPath: string, config: any) {}
+function isFolder(directoryPath: string, config: any) { }
 function apiAutoImport(options: any, targetPath: string, dirPathNname: string) {
     traverseFiles(targetPath, dirPathNname);
 }
